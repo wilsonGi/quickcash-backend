dd4b0f5470dbcb1da1c78b48ba27d272bce7ac0f
wilsonGi <erico1x2@gmail.com>
2025-06-15 13:42:12 +0000

    Add UtilityController to fetch public IP

---
+++ QuickCashJobAPI/Controllers/UtilityController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuickCashJobAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UtilityController : ControllerBase
+    {
+        [HttpGet("get-ip")]
+        public async Task<IActionResult> GetIp()
+        {
+            using var httpClient = new HttpClient();
+            var ip = await httpClient.GetStringAsync("https://ifconfig.me");
+            return Ok(ip);
+        }
+    }
+}
