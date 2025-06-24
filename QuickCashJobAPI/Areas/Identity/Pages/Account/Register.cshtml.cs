55ceb4aa1084a8b2e6a852f15dbf4adba94501d4
wilsonGi <erico1x2@gmail.com>
2025-06-24 08:09:43 +0000

    Permamemt Register Restored

--- QuickCashJobAPI/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ QuickCashJobAPI/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -169,9 +169,7 @@ namespace QuickCashJobAPI.Areas.Identity.Pages.Account
                     {
                         _logger.LogInformation("User created a new account with password.");
                     // Assign the role, defaulting to "Customer" if no role is provided
-                    //string role = string.IsNullOrEmpty(Input.Role) ? SD.Role_Customer : Input.Role;
-                    string role = string.IsNullOrEmpty(Input.Role) ? SD.Role_Admin : Input.Role;
-
+                    string role = string.IsNullOrEmpty(Input.Role) ? SD.Role_Customer : Input.Role;
                     await _userManager.AddToRoleAsync(user, role);
 
 
